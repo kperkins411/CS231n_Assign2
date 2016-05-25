a6dbb873ba98d96068bc27be2a5bfa077005f907
Keith Perkins <kperkins411@gmail.com>
2016-05-25 21:31:42 +0000

    RmsProp and Adam down, Adam had a mysterious step not defined anywhere

--- cs231n/optim.py
+++ cs231n/optim.py
@@ -47,7 +47,6 @@ def sgd(w, dw, config=None):
 def sgd_momentum(w, dw, config=None):
   """
   Performs stochastic gradient descent with momentum.
-
   config format:
   - learning_rate: Scalar learning rate.
   - momentum: Scalar between 0 and 1 giving the momentum value.
@@ -59,20 +58,19 @@ def sgd_momentum(w, dw, config=None):
   config.setdefault('learning_rate', 1e-2)
   config.setdefault('momentum', 0.9)
   v = config.get('velocity', np.zeros_like(w))
-  
-  next_w = None
+
   #############################################################################
   # TODO: Implement the momentum update formula. Store the updated value in   #
   # the next_w variable. You should also use and update the velocity v.       #
   #############################################################################
-  pass
+  v = config['momentum'] * v - config['learning_rate'] * dw # integrate velocity
+  next_w = w+ v # integrate position
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
   config['velocity'] = v
 
   return next_w, config
-
 
 
 def rmsprop(x, dx, config=None):
@@ -92,18 +90,18 @@ def rmsprop(x, dx, config=None):
   config.setdefault('decay_rate', 0.99)
   config.setdefault('epsilon', 1e-8)
   config.setdefault('cache', np.zeros_like(x))
-
-  next_x = None
   #############################################################################
   # TODO: Implement the RMSprop update formula, storing the next value of x   #
   # in the next_x variable. Don't forget to update cache value stored in      #  
   # config['cache'].                                                          #
   #############################################################################
-  pass
+  config['cache'] = config['decay_rate'] * config['cache'] + (1 - config['decay_rate']) * dx**2
+  next_x =x - config['learning_rate'] * dx / (np.sqrt( config['cache']) +  config['epsilon'])
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
-
+  #import pdb
+  #pdb.set_trace()
   return next_x, config
 
 
@@ -136,7 +134,14 @@ def adam(x, dx, config=None):
   # the next_x variable. Don't forget to update the m, v, and t variables     #
   # stored in config.                                                         #
   #############################################################################
-  pass
+  
+  config['m'] = config['beta1'] *config['m'] + (1-config['beta1'])*dx
+  config['v'] = config['beta2']*config['v'] + (1-config['beta2'])*(dx**2)
+  config['t'] = config['t'] + 1
+  #how the hell are you supposed to figure the following out given the course notes?
+  valc_learning_rate = config['learning_rate'] * np.sqrt(1 - config['beta2'] ** config['t']) / (1 -config['beta1'] ** config['t'])
+  next_x = x -valc_learning_rate * config['m'] / (np.sqrt(config['v']) + config['epsilon'])
+
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
